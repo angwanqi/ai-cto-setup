1a50b0062085661bfe567676d83350f826120924
Boo Kian Yeo <bookian@google.com>
2025-04-28 18:19:59 +0800

    add iam bindings for compute engine SA

--- 02-resource-provisioning/terraform/01-base-infra/iam.tf
+++ 02-resource-provisioning/terraform/01-base-infra/iam.tf
@@ -1,5 +1,18 @@
 locals {
   user_list = formatlist("user:%s", var.project_users)
+
+  required_roles = [
+    "roles/artifactregistry.admin",       # Artifact Registry Administrator
+    "roles/bigquery.admin",               # BigQuery Admin
+    "roles/cloudbuild.builds.builder",    # Cloud Build Service Account
+    "roles/storage.admin",                # Storage Admin
+    "roles/aiplatform.admin",             # Vertex AI administrator
+    "roles/aiplatform.featurestoreAdmin", # Vertex AI Feature Store admin
+  ]
+}
+
+data "google_compute_default_service_account" "default" {
+  project = var.project_id
 }
 
 # make users project editors - to tighten if schedule permits
@@ -15,7 +28,17 @@ resource "google_project_iam_custom_role" "lab_users" {
   role_id = "lab_user"
   title   = "lab_user"
   permissions = [
-    "compute.disks.get"
+    "storage.objects.create",
+    "storage.objects.delete",
+    "storage.objects.get",
+    "storage.objects.getIamPolicy",
+    "storage.objects.list",
+    "storage.objects.move",
+    "storage.objects.overrideUnlockedRetention",
+    "storage.objects.restore",
+    "storage.objects.setIamPolicy",
+    "storage.objects.setRetention",
+    "storage.objects.update",
   ]
 }
 
@@ -23,6 +46,24 @@ resource "google_project_iam_custom_role" "lab_users" {
 resource "google_project_iam_binding" "project_users" {
   project = var.project_id
   role    = google_project_iam_custom_role.lab_users.name
-
   members = local.user_list
+}
+
+# bind some built-in roles to users
+resource "google_project_iam_binding" "project_user_required_roles" {
+  for_each = toset(local.required_roles)
+
+  project = var.project_id
+  role    = each.value
+  members = local.user_list
+}
+
+# bind roles to default compute service account
+resource "google_project_iam_binding" "compute_engine_required_roles" {
+  for_each = toset(local.required_roles)
+  project  = var.project_id
+  role     = each.value
+  members = [
+    "serviceAccount:${data.google_compute_default_service_account.default.email}",
+  ]
 }
